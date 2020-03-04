88f6240f685546e79cbd5d6c7b4e47d8a5c44a68
Pere <pere.solaclaver@gmail.com>
2020-03-04 23:14:09 +0000

    Place rooms in a x y grid, add x and y columns in model and save them to database

--- util/create_world.py
+++ util/create_world.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from adventure.models import Player, Room, World
+import random
 
 Room.objects.all().delete()
 
@@ -14,3 +15,19 @@ players=Player.objects.all()
 for p in players:
   p.currentRoom=room_array[0].id
   p.save()
+# numbers = []
+
+# world = Room.objects.all()
+# for el in world:
+#   numbers.append(el.id)
+
+# length_num = len(numbers)
+# rand_num = random.randrange(0, length_num)
+
+# for room in world:
+
+
+
+
+
+
