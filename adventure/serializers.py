564e30bebb4cec7613e41df68d0a48ef94db7c3b
Pere <pere.solaclaver@gmail.com>
2020-03-05 15:24:29 +0000

    Order rooms by id returned by endpoint

--- adventure/serializers.py
+++ adventure/serializers.py
@@ -4,6 +4,7 @@ from .models import Room, Player
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
+        ordering = ['id']
         fields = ['id', 'title', 'description', 'n_to', 's_to', 'e_to', 'w_to', 'x', 'y']
 
 class PlayerSerializer(serializers.ModelSerializer):
